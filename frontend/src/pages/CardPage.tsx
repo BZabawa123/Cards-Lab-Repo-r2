53de1d864b08a2cc129a2dadd7c50a2ba2a3dc82
BZabawa123 <br223097@ucf.edu>
2025-04-15 12:50:26 +0000

    ALl my latest changes to my personal Cards Lab Repo

--- frontend/src/pages/CardPage.tsx
+++ frontend/src/pages/CardPage.tsx
@@ -1,6 +1,9 @@
-import PageTitle from '../components/PageTitle.tsx';
-import LoggedInName from '../components/LoggedInName.tsx';
-import CardUI from '../components/CardUI.tsx';
+// import PageTitle from '../components/PageTitle.tsx';
+// import LoggedInName from '../components/LoggedInName.tsx';
+// import CardUI from '../components/CardUI.tsx';
+import PageTitle from '../components/PageTitle';
+import LoggedInName from '../components/LoggedInName';
+import CardUI from '../components/CardUI';
 
 const CardPage = () => {
     return (
